c83e926aa64467a584ec51d5d1c6ef123a952c12
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2022-07-01 19:45:23 +0800

    Chore: Rename Countries to Main

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Header from "./components/header/Header";
-import Countries from "./containers/countries/Countries";
+import Main from "./containers/main/Main";
 
 export default function App() {
   return (
     <>
       <Header />
 
-      <Countries />
+      <Main />
     </>
   );
 }
--- src/containers/main/Main.js
+++ src/containers/main/Main.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Form from "../../components/form/Form";
 
-export default function Countries() {
+export default function Main() {
   return (
     <main>
       <Form />
