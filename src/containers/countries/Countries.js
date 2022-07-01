a376d8b19d2ab9872ba91c144df872c5c06aea7a
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2022-07-01 12:56:40 +0800

    Feat: Add Contries container to encapsulate the main content
    It will hold all logic relating to fetching data and displaying search results.

---
+++ src/containers/countries/Countries.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Form from "../../components/form/Form";
+
+export default function Countries() {
+  return (
+    <main>
+      <Form />
+      <h1>Hello World!</h1>
+    </main>
+  );
+}
