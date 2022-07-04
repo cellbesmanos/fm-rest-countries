160eae030a493d03a7869bbb129d699a512a2b19
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2022-07-04 16:41:20 +0800

    Feat: Change Country component to accept dynamic data

--- src/components/country/Country.js
+++ src/components/country/Country.js
@@ -1,26 +1,26 @@
 import React from "react";
 import "./Country.css";
 
-export default function Country() {
+export default function Country({ img, name, population, region, capital }) {
   return (
     <div className="Country">
-      <img src="" alt="Country yey" className="Country__img" />
+      <img src={img} alt={`flag of ${name}`} className="Country__img" />
 
       <div className="Country__info">
-        <h2 className="Country__name">Country name</h2>
+        <h2 className="Country__name">{name}</h2>
 
         <div className="Country__stats">
           <div>
             <h3>Population:</h3>
-            <span>81,770,900</span>
+            <span>{population}</span>
           </div>
           <div>
             <h3>Region:</h3>
-            <span>Europe</span>
+            <span>{region}</span>
           </div>
           <div>
             <h3>Capital:</h3>
-            <span>Berlin</span>
+            <span>{capital || "N/A"}</span>
           </div>
         </div>
       </div>
