e2969a22ecf8e61589c695a2c6e4c58cb389da63
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2022-07-04 15:39:37 +0800

    Fix: Remove timeout and change error handling

--- src/useFetch.js
+++ src/useFetch.js
@@ -5,30 +5,34 @@ export default function useFetch(url) {
 
   useEffect(() => {
     const controller = new AbortController();
-    const timeoutMsg =
-      "Connection timeout. Please check your internet connection";
 
     (async () => {
       try {
         const res = await fetch(url, { signal: controller.signal });
-        const timeoutID = setTimeout(() => controller.abort(timeoutMsg), 2000);
 
         if (res.status !== 200) {
-          throw new Error(res.statusText);
-        } else if (res.status === 200) {
-          clearTimeout(timeoutID);
+          const error = new Error(
+            "That does not seem right. Please try a different keyword."
+          );
+          error.name = "NotFound";
+          throw error;
         }
 
         const json = await res.json();
         setStatus({ data: json, loading: false });
       } catch (err) {
-        throw err;
+        if (err.name === "AbortError") {
+          throw new Error("Something went wrong.");
+        } else {
+          throw err;
+        }
       }
     })();
 
     // cancel request if component unmounts
-    return () =>
-      controller.abort("Cannot finish the request. Component was unmounted.");
+    return () => {
+      controller.abort();
+    };
   }, [url]);
 
   return status;
