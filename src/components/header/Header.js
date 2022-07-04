229569cb224dafbe5bee40399e3cfff58b0a93c2
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2022-07-04 09:13:35 +0800

    Fix: Edit Header and main layout for large screens
    To avoid uneccesarily large layout on large screens. Change padding inline of main to a fix number than clamp() to be predictable in any screen size.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,19 +6,19 @@ export default function Header() {
 
   return (
     <header className="Header">
-      <p className="Header__logo">Where in the world?</p>
-
-      <div className="Header__controls">
-        <button
-          className="Header__toggle"
-          type="button"
-          role="switch"
-          aria-label="dark mode toggle"
-          aria-checked={isDarkMode}
-          onClick={() => setIsDarkMode((prev) => !prev)}
-        ></button>
-
-        <p>Dark Mode</p>
+      <div className="Header__container">
+        <p className="Header__logo">Where in the world?</p>
+        <div className="Header__controls">
+          <button
+            className="Header__toggle"
+            type="button"
+            role="switch"
+            aria-label="dark mode toggle"
+            aria-checked={isDarkMode}
+            onClick={() => setIsDarkMode((prev) => !prev)}
+          ></button>
+          <p>Dark Mode</p>
+        </div>
       </div>
     </header>
   );
