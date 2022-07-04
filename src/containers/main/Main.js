14a06397c30397095830b13e06ba4088d1df670b
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2022-07-04 09:21:22 +0800

    Feat: Prepare Main for layout testing

--- src/containers/main/Main.js
+++ src/containers/main/Main.js
@@ -1,14 +1,19 @@
 import React from "react";
 import Country from "../../components/country/Country";
 import Form from "../../components/form/Form";
+import "./Main.css";
 
 export default function Main() {
   return (
     <main>
       <Form />
-      <h1>Hello World!</h1>
+      {/* <h1>Hello World!</h1> */}
 
-      <div>
+      <div className="Main__country-container">
+        <Country />
+        <Country />
+        <Country />
+        <Country />
         <Country />
       </div>
     </main>
