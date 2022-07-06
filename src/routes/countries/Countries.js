da777469e7ac393bfda90819f4a5cccc1f96d5a8
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2022-07-06 16:14:10 +0800

    Feat: Add fetch on mount in Countries container

--- src/routes/countries/Countries.js
+++ src/routes/countries/Countries.js
@@ -1,6 +1,23 @@
 import React from "react";
 import "./Countries.css";
+import useFetch from "../../useFetch";
+import Country from "../../components/country/Country";
 
 export default function Countries() {
-  return <div>Countries</div>;
+  const baseURL = "https://restcountries.com/v3.1";
+  const responseFilter = "fields=name,flags,population,region,capital";
+  const { data: countries, loading } = useFetch(
+    `${baseURL}/all?${responseFilter}`
+  );
+
+  return (
+    <div>
+      <h1>This is the countries container!</h1>
+      {loading
+        ? "Fetching data..."
+        : countries.map((country) => (
+            <Country key={country.name.common} name={country.name.common} />
+          ))}
+    </div>
+  );
 }
