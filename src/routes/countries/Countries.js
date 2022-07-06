c789c24fe1816af8336027979ca770e0970a39dd
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2022-07-06 16:25:50 +0800

    Import Form component

--- src/routes/countries/Countries.js
+++ src/routes/countries/Countries.js
@@ -3,6 +3,7 @@ import "./Countries.css";
 import useFetch from "../../useFetch";
 import Country from "../../components/country/Country";
 import CountryList from "../../components/countryList/CountryList";
+import Form from "../../components/form/Form";
 
 export default function Countries() {
   const baseURL = "https://restcountries.com/v3.1";
@@ -12,13 +13,17 @@ export default function Countries() {
   );
 
   return (
-    <CountryList>
-      <h1>This is the countries container!</h1>
-      {loading
-        ? "Fetching data..."
-        : countries.map((country) => (
-            <Country key={country.name.common} name={country.name.common} />
-          ))}
-    </CountryList>
+    <main>
+      <Form />
+
+      <CountryList>
+        <h1>This is the countries container!</h1>
+        {loading
+          ? "Fetching data..."
+          : countries.map((country) => (
+              <Country key={country.name.common} name={country.name.common} />
+            ))}
+      </CountryList>
+    </main>
   );
 }
