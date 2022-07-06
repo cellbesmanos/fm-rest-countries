284cd63aa3894bc7521c9fde673ac1e53f4221c4
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2022-07-06 15:57:26 +0800

    Feat: Add react-router-dom@6 as dependency
    Establish routes for index and countries

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
+import Countries from "./routes/countries/Countries";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/countries" element={<Countries />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
