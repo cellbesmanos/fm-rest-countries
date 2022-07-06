4a9bf75aebabf134bbf9cdffc72449fd7d57904d
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2022-07-06 16:41:26 +0800

    Fix: Remove useNagivate hook
    On mount, it automatically redirects back to itself resulting in never changing page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import React, { useEffect } from "react";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Header from "./components/header/Header";
 
 export default function App() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    navigate("/countries", { replace: true });
-  }, [navigate]);
-
   return (
     <div>
       <Header />
