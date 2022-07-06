075598bbbc543d5779967395ce88ade52a5ccc56
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2022-07-06 16:16:33 +0800

    Feat: Wrap mapped fetch results with CountryList

--- src/routes/countries/Countries.js
+++ src/routes/countries/Countries.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Countries.css";
 import useFetch from "../../useFetch";
 import Country from "../../components/country/Country";
+import CountryList from "../../components/countryList/CountryList";
 
 export default function Countries() {
   const baseURL = "https://restcountries.com/v3.1";
@@ -11,13 +12,13 @@ export default function Countries() {
   );
 
   return (
-    <div>
+    <CountryList>
       <h1>This is the countries container!</h1>
       {loading
         ? "Fetching data..."
         : countries.map((country) => (
             <Country key={country.name.common} name={country.name.common} />
           ))}
-    </div>
+    </CountryList>
   );
 }
