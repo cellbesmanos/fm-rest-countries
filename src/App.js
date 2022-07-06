9072bc8f4f31ef6078ec6a4604abeb94d871a7b1
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2022-07-06 15:59:31 +0800

    Make App redirect on mount to path coutries

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import Header from "./components/header/Header";
 
 export default function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/countries", { replace: true });
+  }, [navigate]);
+
   return (
     <div>
-      <h1>Hello World!</h1>
+      <Header />
+
+      <Outlet />
     </div>
   );
 }
