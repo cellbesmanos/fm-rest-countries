458cc2c77334ee190dbe3318f29bfffe649dbfeb
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2022-07-07 17:15:35 +0800

    Fix: Add error handling
    Throw error when there is no match found/accessing incorrect endpoints and manual timeout.

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,16 +1,68 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 export default function useFetch(url) {
-  const [status, setStatus] = useState({ data: [], loading: true });
+  const [status, setStatus] = useState({
+    data: [],
+    loading: true,
+    error: null,
+  });
+  let timeoutTimer = useRef(null);
 
   useEffect(() => {
-    (async () => {
-      const response = await fetch(url);
-      const json = await response.json();
+    timeoutTimer.current = setTimeout(() => {
+      const error = new Error(
+        "Connection timeout. Please check your internet connection."
+      );
+      error.name = "NetworkError";
+      setStatus((prev) => {
+        return { ...prev, error: error };
+      });
+    }, 8000);
+  }, []);
 
-      setStatus({ data: json, loading: false });
+  useEffect(() => {
+    const controller = new AbortController();
+
+    (async () => {
+      try {
+        const response = await fetch(url, { signal: controller.signal });
+
+        if (response.status !== 200) {
+          const error = new Error(
+            "That does not seem right. Please use a country's name or its partial name."
+          );
+          error.name = "NotFound";
+
+          throw error;
+        }
+        clearTimeout(timeoutTimer.current);
+        const json = await response.json();
+
+        setStatus({ ...status, data: json, loading: false });
+      } catch (error) {
+        if (error.name === "AbortError") {
+          console.warn(
+            "Fetch request successfully aborted. You can safely ignore this message."
+          );
+        } else if (error.name === "NotFound") {
+          clearTimeout(timeoutTimer.current);
+          setStatus((prev) => {
+            return {
+              ...prev,
+              error: error,
+            };
+          });
+        } else {
+          console.error(error.message);
+        }
+      }
     })();
-  }, [url]);
+
+    return () => {
+      controller.abort();
+      clearTimeout(timeoutTimer.current);
+    };
+  }, [url, status]);
 
   return status;
 }
