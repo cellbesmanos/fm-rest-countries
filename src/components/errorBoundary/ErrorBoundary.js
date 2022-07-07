ad897cfc8f4a586a787b94bb9ffc4e5e8755732b
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2022-07-07 17:14:51 +0800

    Feat: Add ErrorBoundary component

---
+++ src/components/errorBoundary/ErrorBoundary.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+export default class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false,
+      msg: "",
+    };
+  }
+
+  static getDerivedStateFromError(error) {
+    return {
+      hasError: true,
+    };
+  }
+
+  componentDidCatch(error, info) {
+    this.setState({
+      msg: error.message,
+    });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <p>{this.state.msg}</p>;
+    }
+
+    return this.props.children;
+  }
+}
