c06c4b152edf5be0a08535904d814407c48398ce
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2022-07-07 17:37:54 +0800

    Feat: Move useFetch logic to DetailsContent
    To easily wrap it with ErrorBoundary. In case of fallback the back button will remain.

--- src/routes/details/Details.js
+++ src/routes/details/Details.js
@@ -1,71 +1,19 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
-import Borders from "../../components/borders/Borders";
-import useFetch from "../../useFetch";
+
+import DetailsContent from "../../components/detailsContent/DetailsContent";
+
 import "./Details.css";
 
 export default function Details() {
   const { id } = useParams();
-  const baseURL = "https://restcountries.com/v3.1/alpha";
-  const responseFilter =
-    "fields=name,flags,population,region,subregion,capital,tld,currencies,languages,borders";
-  const { data: country, loading } = useFetch(
-    `${baseURL}/${id}?${responseFilter}`
-  );
-
-  function extractObject(obj, type) {
-    if (type !== "lang" && type !== "curr") {
-      throw new Error("Cannot extract object of invalid type.");
-    }
-
-    let index = 0;
-    let str = "";
-    let concat = "";
-    for (const key of Object.keys(obj)) {
-      if (index === 0) {
-        concat = type === "lang" ? obj[key] : obj[key].name;
-      } else {
-        concat = type === "lang" ? `, ${obj[key]}` : `, ${obj[key].name}`;
-      }
-      str += concat;
-      index++;
-    }
-    return str;
-  }
 
   return (
     <div>
       <Link to="/">Back</Link>
       <div>Details</div>
 
-      {loading ? (
-        "Fetching data..."
-      ) : (
-        <div>
-          <img src={country.flags.png} alt={`flag of ${country.name.common}`} />
-          <div>
-            <p>{country.name.common}</p>
-            <p>{country.name.official}</p>
-            <p>{country.population.toLocaleString("en-US")}</p>
-            <p>{country.region}</p>
-            <p>{country.subregion}</p>
-            <p>{country.capital}</p>
-          </div>
-          <div>
-            <p>{country.tld}</p>
-            <p>{extractObject(country.currencies, "curr")}</p>
-            <p>{extractObject(country.languages, "lang")}</p>
-          </div>
-
-          <div>
-            {country.borders.length > 0 ? (
-              <Borders borders={country.borders} />
-            ) : (
-              "No border countries."
-            )}
-          </div>
-        </div>
-      )}
+      <DetailsContent id={id} />
     </div>
   );
 }
