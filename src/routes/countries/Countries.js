71fc27588b0e5cc95c5bc7c3bf4bd911df7a2caa
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2022-07-07 17:24:58 +0800

    Feat: Move useFetch logic to CoutryList component
    It make it easier to wrap it with ErrorBoundary so the Form will stay while the list will render the fallback.

--- src/routes/countries/Countries.js
+++ src/routes/countries/Countries.js
@@ -1,8 +1,6 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import useFetch from "../../useFetch";
+import React, { useState } from "react";
 import "./Countries.css";
-import Country from "../../components/country/Country";
+
 import CountryList from "../../components/countryList/CountryList";
 import Form from "../../components/form/Form";
 
@@ -10,24 +8,13 @@ export default function Countries() {
   const baseURL = "https://restcountries.com/v3.1";
   const responseFilter = "fields=name,cca3,flags,population,region,capital";
 
-  const { data: countries, loading } = useFetch(
-    `${baseURL}/all?${responseFilter}`
-  );
+  const [url, setURL] = useState(`${baseURL}/all?${responseFilter}`);
 
   return (
     <main>
       <Form />
 
-      <CountryList>
-        <h1>This is the countries container!</h1>
-        {loading
-          ? "Fetching data..."
-          : countries.map((country) => (
-              <Link key={country.name.common} to={`/country/${country.cca3}`}>
-                <Country name={country.name.common} />
-              </Link>
-            ))}
-      </CountryList>
+      <CountryList url={url} />
     </main>
   );
 }
