7db6e8040d16894fd1df856896002ad64124604b
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2022-07-08 13:21:04 +0800

    Fix: Remove dark mode toggle

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,24 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Header.css";
 
 export default function Header() {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
   return (
     <header className="Header">
       <div className="Header__container">
         <p className="Header__logo">Where in the world?</p>
-        <div className="Header__controls">
-          <button
-            className="Header__toggle"
-            type="button"
-            role="switch"
-            aria-label="dark mode toggle"
-            aria-checked={isDarkMode}
-            onClick={() => setIsDarkMode((prev) => !prev)}
-          ></button>
-          <p>Dark Mode</p>
-        </div>
       </div>
     </header>
   );
